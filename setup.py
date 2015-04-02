1c89bfb41d883f121ed4a40cfc17319ab835d04a
Paul Water <p.r.water@setbased.nl>
2015-04-02 13:27:48 +0200

    Release: 0.9.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='PyStratum',
 
-    version='0.1.16',
+    version='0.9.0',
 
     description='A stored procedure and function loader, wrapper generator for MySQL and MS-SQL',
     long_description=long_description,
