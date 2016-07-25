4ecefe21cb1e19d78e98ca60c0fa7302b5a5e3bc
Paul Water <p.r.water@setbased.nl>
2016-07-25 10:49:00 +0200

    Release: 0.10.1

--- pystratum/application/PyStratumApplication.py
+++ pystratum/application/PyStratumApplication.py
@@ -23,7 +23,7 @@ class PyStratumApplication(Application):
         """
         Object constructor
         """
-        Application.__init__(self, 'pystratum', '0.9.21')
+        Application.__init__(self, 'pystratum', '0.10.1')
 
     # ------------------------------------------------------------------------------------------------------------------
     def get_default_commands(self):
