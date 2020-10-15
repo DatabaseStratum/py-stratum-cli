1d8eaff3979cd90af1e601ee03cbd0a015b0264b
Paul Water <p.r.water@setbased.nl>
2020-10-15 12:42:47 +0200

    Fix build.

--- pystratum_cli/application/StratumApplication.py
+++ pystratum_cli/application/StratumApplication.py
@@ -18,7 +18,7 @@ class StratumApplication(Application):
         """
         Object constructor
         """
-        Application.__init__(self, 'pystratum', '0.0.0')
+        Application.__init__(self, 'pystratum', '1.0.0')
 
     # ------------------------------------------------------------------------------------------------------------------
     def get_default_commands(self) -> List[Command]:
