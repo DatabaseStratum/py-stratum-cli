c7b2f18ad967ab7b82df176d8e585fdd6cf8e5b8
Paul Water <p.r.water@setbased.nl>
2020-10-15 12:40:59 +0200

    Release: 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='PyStratum-Cli',
 
-    version='0.0.0',
+    version='1.0.0',
 
     description='PyStratum: Command Line Interface',
     long_description=long_description,
